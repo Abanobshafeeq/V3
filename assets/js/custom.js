43428add72880152b3aa95464269cddb249f45e7
Abanoub <abanoub20010@feng.bu.edu.eg>
2025-03-29 14:57:08 +0200

    f

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,7 @@
 $(document).ready(function(){
 	"use strict";
     
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : Listrace directory HTML Template
-* Version       : 1.0
-==================================== */
-
-
-
-
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top 
-2. slick carousel
-3. welcome animation support
-4. feather icon
-5. counter
-======================================*/
-
+    
     // 1. Scroll To Top 
 		$(window).on('scroll',function () {
 			if ($(this).scrollTop() > 600) {
