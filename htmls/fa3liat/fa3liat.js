20183f3ed41055cf618bed23d8fcf0d1e8fe0f1f
Abanoub <abanoub20010@feng.bu.edu.eg>
2025-04-06 00:06:36 +0200

    hh

--- htmls/fa3liat/fa3liat.js
+++ htmls/fa3liat/fa3liat.js
@@ -46,3 +46,29 @@ async function fetchImages2015() {
 }
 
 fetchImages2015();
+//////////////////////////////////////////////////////////////////////////
+
+function toggleDropdownw2015() {
+    const content = document.getElementById("dropdownContentw2015");
+    const arrow = document.getElementById("arrow");
+    content.classList.toggle("show");
+    arrow.classList.toggle("rotate");
+}
+async function fetchImagesw2015() {
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbx9XaF0-dAbjtmjZ7FKLGn8w-c5rVj7NjwCtYXv6ioXPnjybxfTJx3aF87Malkb_HK08w/exec"; // ضع رابط Web App هنا
+    try {
+        const response = await fetch(scriptUrl);
+        const imageLinks = await response.json();
+
+        const gallery = document.getElementById("imageGalleryw2015");
+        imageLinks.forEach(link => {
+            const img = document.createElement("img");
+            img.src = link;
+            gallery.appendChild(img);
+        });
+    } catch (error) {
+        console.error("حدث خطأ أثناء تحميل الصور:", error);
+    }
+}
+
+fetchImagesw2015();
