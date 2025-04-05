bdae109782a657a129620eabcb6878d904f65eea
Abanoub <abanoub20010@feng.bu.edu.eg>
2025-04-05 14:26:47 +0200

    asdadad

--- htmls/fa3liat/fa3liat.js
+++ htmls/fa3liat/fa3liat.js
@@ -22,3 +22,27 @@ async function fetchImages() {
 }
 
 fetchImages();
+function toggleDropdown2015() {
+    const content = document.getElementById("dropdownContent2015");
+    const arrow = document.getElementById("arrow");
+    content.classList.toggle("show");
+    arrow.classList.toggle("rotate");
+}
+async function fetchImages2015() {
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbyC3YSj9dHhCerigBSbfSZtNB7IJMvCJ1Wn_sMTsFAeGpelLYQr-Q5I6l-dvjS2XmwuaA/exec"; // ضع رابط Web App هنا
+    try {
+        const response = await fetch(scriptUrl);
+        const imageLinks = await response.json();
+
+        const gallery = document.getElementById("imageGallery2015");
+        imageLinks.forEach(link => {
+            const img = document.createElement("img");
+            img.src = link;
+            gallery.appendChild(img);
+        });
+    } catch (error) {
+        console.error("حدث خطأ أثناء تحميل الصور:", error);
+    }
+}
+
+fetchImages2015();
